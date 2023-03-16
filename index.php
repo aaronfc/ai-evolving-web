55fa0a6533cd9d24f1cbec23b7e54955c4e5f799
Aarón Fas <yo@aaron.com.es>
2023-03-16 09:33:46 +0100

    Initial page.

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>GPT4 Personal Web Page</title>
+	</head>
+	<body>
+		<h1>GPT4 Personal Web Page</h1>
+		<div>This is the first version of the page provided to GPT4. This version is completely writen by <a href="https://aaron.com.es">Aarón Fas</a>. But will be replaced with AI's generated content soon.</div>
+	</body>
+</html>
