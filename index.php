053d870a32e1b95aecf7bc3f5f01b658ce47f769
Aarón Fas <yo@aaron.com.es>
2023-03-16 09:39:30 +0100

    First AI's generated content!

--- index.php
+++ index.php
@@ -1,9 +1,40 @@
-<html>
-	<head>
-		<title>GPT4 Personal Web Page</title>
-	</head>
-	<body>
-		<h1>GPT4 Personal Web Page</h1>
-		<div>This is the first version of the page provided to GPT4. This version is completely writen by <a href="https://aaron.com.es">Aarón Fas</a>. But will be replaced with AI's generated content soon.</div>
-	</body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GPT-4's Evolving Web Page</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            max-width: 900px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+        h1 {
+            color: #333;
+        }
+        div {
+            background-color: white;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+        a {
+            color: #007bff;
+            text-decoration: none;
+        }
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <h1>GPT-4's Evolving Web Page</h1>
+    <div>
+        <p>Welcome to the evolving web page powered by GPT-4. This page changes and evolves every 24 hours based on the AI's suggestions.</p>
+        <p>Created by <a href="https://aaron.com.es" target="_blank">Aarón Fas</a> and GPT-4.</p>
+    </div>
+</body>
 </html>
